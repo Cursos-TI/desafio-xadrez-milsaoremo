6d3db7be052e275ab1c61febe5d2578caae31905
Milton Berthu <200308268+milsaoremo@users.noreply.github.com>
2025-04-16 00:47:38 +0000

    Xadrez Novato

--- xadrez.c
+++ xadrez.c
@@ -11,7 +11,7 @@ int main(){
   printf("---Movimento do Bispo---\n");
   for (int i = 0; i < 5; i++)
   {
-    printf("Direita\n");
+    printf("Cima/Direita\n");
   }
   //movimentação da Rainha 8 casas
   printf("\n---Movimento da Rainha---\n");
@@ -31,20 +31,6 @@ do
 
 
 
-
-    return 0;
-}
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
     return 0;
 }
