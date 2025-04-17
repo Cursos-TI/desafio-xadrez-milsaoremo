5454fc1a954a8b930affaf2023b98a1b73362ad0
Milton Berthu <200308268+milsaoremo@users.noreply.github.com>
2025-04-17 01:23:59 +0000

    Xadrez Novato

--- xadrez.c
+++ xadrez.c
@@ -8,20 +8,20 @@ int main(){
 
 
 //movimentação do bispo 5 casas
-  printf("---Movimento do Bispo---\n");
+  printf("--- Movimento do Bispo ---\n");
   for (int i = 0; i < 5; i++)
   {
     printf("Cima/Direita\n");
   }
   //movimentação da Rainha 8 casas
-  printf("\n---Movimento da Rainha---\n");
+  printf("\n--- Movimento da Rainha ---\n");
 while (rainha <=8)
 {
   printf("Esquerda\n", rainha);
   rainha++;
 }
 //movimentação da Torre 5 casas
-  printf("\n---Movimento da Torre---\n");
+  printf("\n--- Movimento da Torre ---\n");
 
 do
 {
@@ -29,6 +29,19 @@ do
     torre++;
 } while (torre <= 5);
 
+int cavalo = 1;
+
+printf("\n--- Movimento do Cavalo ---\n");
+
+while (cavalo--)
+{
+  for (int i = 0; i < 2; i++)
+  {
+    printf("Cima\n"); // Imprime "cima " duas vezes
+    
+  }
+  printf("Direita\n"); // Imprime Direita 1 vez
+}
 
 
 
